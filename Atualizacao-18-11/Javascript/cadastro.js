ce6a079fb722b1d86e1161194a053cac0b66550e
Lucas Silva <99055681+sirlucasw@users.noreply.github.com>
2025-11-18 04:54:08 -0300

    Atualizacao 18-11
    Atualizações 18/11:
    
    1. Botões de cadastro da tela inicial da Plataforma para a página de Login;
    2. Na página de Login, campo "esqueci minha senha" redirecionando para a página recuperação de senha;
    3. Botão "cadaster-se" redirecionando para a página de cadastro CNPJ/CPF;
    4. Adição do JavaScript para transição entre as telas (Index e cadastro);
    5. Botões de redes sociais (rodapé) redirecionamento para os perfis da BBTS (Instagram, LinkedIn e Facebook);
    6. Logo "clickável" que redireciona o usuário para o início da página

---
+++ Atualizacao-18-11/Javascript/cadastro.js
@@ -0,0 +1,3 @@
+document.getElementById("login-link")?.addEventListener("click", () => {
+  window.location.href = "cadastrar.html";
+});
---
+++ Atualizacao-18-11/Javascript/index.js
@@ -0,0 +1,19 @@
+// Botão do topo
+document.getElementById("botao-topo")?.addEventListener("click", () => {
+  window.location.href = "cadastrar.html";
+});
+
+// Botão dos cards
+document.getElementById("botao-comecar")?.addEventListener("click", () => {
+  window.location.href = "cadastrar.html";
+});
+
+// Botão de benefícios
+document.getElementById("botao-cadastro")?.addEventListener("click", () => {
+  window.location.href = "cadastrar.html";
+});
+
+// Botão depoimentos
+document.getElementById("botao-depoimentos")?.addEventListener("click", () => {
+  window.location.href = "cadastrar.html";
+});
